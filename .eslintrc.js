222d77f73284614a07f8ccf5b0b4d80a0f3d1ce4
Maki <loudnownet@gmail.com>
2019-12-20 16:49:56 +0100

    fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     '@vue/standard',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'comma-dangle': 'off',
     'no-new-object': 'off',
     'no-unmodified-loop-condition': 'off',
